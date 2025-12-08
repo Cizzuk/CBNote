97715ad7a21dd6c1552e785a53bacb9714013d8e
Cizzuk <dropcat@cizzuk.net>
2025-12-08 14:11:48 +0900

    Update About

--- CBNote/Views/SettingsView.swift
+++ CBNote/Views/SettingsView.swift
@@ -68,30 +68,27 @@ struct SettingsView: View {
                 
                 // App Information
                 Section {
-                    Text("CBNote")
-                        .textSelection(.enabled)
                     HStack {
                         let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
                         let currentBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
-                        Text("Version")
+                        Label("Version", systemImage: "info.circle")
                         Spacer()
                         Text("\(currentVersion ?? "Unknown") (\(currentBuild ?? "Unknown"))")
                             .foregroundColor(.secondary)
                             .textSelection(.enabled)
                     }
                     .accessibilityElement(children: .combine)
-                    HStack {
-                        Text("Developer")
-                        Spacer()
-                        Link(destination:URL(string: "https://cizzuk.net/")!, label: {
-                            Text("Cizzuk")
-                        })
-                    }
                     Link(destination:URL(string: "https://i.cizzuk.net/privacy/")!, label: {
-                        Text("Privacy Policy")
+                         Label("Privacy Policy", systemImage: "hand.raised")
+                    })
+                    Link(destination:URL(string: "https://github.com/Cizzuk/CBNote")!, label: {
+                         Label("Source", systemImage: "ladybug")
                     })
                 } header: {
                     Text("About")
+                } footer: {
+                    Text("MIT License\n\nCopyright (c) 2025 Cizzuk\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.")
+                        .textSelection(.enabled)
                 }
             }
             .navigationTitle("Settings")
