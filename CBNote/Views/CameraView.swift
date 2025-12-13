d463f1f96e3e0eafee343e6d37267d58e52dc076
Cizzuk <dropcat@cizzuk.net>
2025-12-14 01:10:40 +0900

    Update CameraView.swift

--- CBNote/Views/CameraView.swift
+++ CBNote/Views/CameraView.swift
@@ -29,6 +29,7 @@ struct CameraView: View {
                     .multilineTextAlignment(.center)
                 #else
                 if viewModel.cameraPermission == .authorized {
+                    ProgressView()
                     CameraPreview(session: viewModel.session) { point in
                         viewModel.focus(at: point)
                     }
