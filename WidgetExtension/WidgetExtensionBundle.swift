c58802b57be0d8156a5a1981d0aab73dc39499fd
Cizzuk <dropcat@cizzuk.net>
2026-02-16 17:33:58 +0900

    Add StartRecording intent

--- WidgetExtension/WidgetExtensionBundle.swift
+++ WidgetExtension/WidgetExtensionBundle.swift
@@ -14,6 +14,7 @@ struct WidgetExtensionBundle: WidgetBundle {
         OpenAppLaunchCameraControl()
         OpenAppPasteFromClipboardControl()
         OpenAppAddNewNoteControl()
+        OpenAppStartRecordingControl()
         OpenAppOpenAppOnlyControl()
     }
 }
