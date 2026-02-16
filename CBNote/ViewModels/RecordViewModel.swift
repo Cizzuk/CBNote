ba312f380d2b7156cab41107ef4c0a94bef0e9f1
Cizzuk <dropcat@cizzuk.net>
2026-02-16 12:51:26 +0900

    Create RecordView

---
+++ CBNote/ViewModels/RecordViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecordViewModel.swift
+//  CBNote
+//
+//  Created by Cizzuk on 2026/02/16.
+//
+
+import AVFoundation
+import Combine
+import SwiftUI
+
+class RecordViewModel: ObservableObject {
+    
+}
---
+++ CBNote/Views/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  CBNote
+//
+//  Created by Cizzuk on 2026/02/16.
+//
+
+import AVFoundation
+import SwiftUI
+
+struct RecordView: View {
+    @Environment(\.dismiss) private var dismiss
+    @ObservedObject var viewModel: RecordViewModel
+    
+    var body: some View {
+        
+    }
+}
