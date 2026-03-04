71504b2cb4c74e0ce38ffd0bd009a2ccd793db32
Cizzuk <dropcat@cizzuk.net>
2026-03-04 16:47:13 +0900

    Handle Audio Session Interruptions

--- CBNote/Views/RecorderView.swift
+++ CBNote/Views/RecorderView.swift
@@ -58,6 +58,16 @@ struct RecorderView: View {
             .onReceive(NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)) { _ in
                 finishRecordingAndDismiss()
             }
+            .onReceive(NotificationCenter.default.publisher(for: AVAudioSession.interruptionNotification)) { notification in
+                guard let userInfo = notification.userInfo,
+                      let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
+                      let type = AVAudioSession.InterruptionType(rawValue: typeValue)
+                else { return }
+                
+                if type == .began {
+                    finishRecordingAndDismiss()
+                }
+            }
         }
         .background(
             LinearGradient(
