089e435f1f3c1d832578a5dccd97f05921dd0b98
Cizzuk <dropcat@cizzuk.net>
2026-03-07 23:10:30 +0900

    Create Constants.swift

---
+++ Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  CBNote
+//
+//  Created by Cizzuk on 2026/03/07.
+//
+
+import Foundation
+
+let groupUserDefaults = UserDefaults(suiteName: "group.net.cizzuk.cbnote")!
+
+enum GUDFlag {
+    static let shouldFinishRecording = "flag_shouldFinishRecording"
+}
